dd4c8945724ffaf55e97204431096b0b564cf689
SupaFresh <221895@c1mrxb2qh3qd.isd720.com>
2017-11-17 13:45:09 -0600

    everything
    yep

---
+++ myWebsite/javascript/myWebsite.js
@@ -0,0 +1,7 @@
+function changeText1() {
+	document.getElementById("hello").innerHTML="javascript because why not";
+}
+
+function changeText1R() {
+	document.getElementById("hello").innerHTML="why hello there";
+}
